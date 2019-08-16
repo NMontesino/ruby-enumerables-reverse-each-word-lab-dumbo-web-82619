6ec73b53f0b8ae8fb3c9c8a82c11557d7f4ca902
glorious-genre-8033 <namontesino@gmail.com>
2019-08-16 23:39:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,13 @@
 def reverse_each_word(string)
-  sentence_array = string.split(' ')
-  reversed_array = sentence_array.each{|word| word.reverse!}
-  reversed_array.join(' ')
+  
 end
+
+string.split(' ').each{|word| word.reverse!}.join(' ')
 
 def reverse_each_word(string)
   
 end
+
+sentence_array = string.split(' ')
+  reversed_array = sentence_array.each{|word| word.reverse!}
+  reversed_array.join(' ')
