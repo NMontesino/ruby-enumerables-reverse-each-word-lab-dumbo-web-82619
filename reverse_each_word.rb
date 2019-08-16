03f99ddacefa67d1db47e0d7b1e3023f7fb883cc
glorious-genre-8033 <namontesino@gmail.com>
2019-08-16 23:34:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(string)
   sentence_array = %w(string)
-  sentence_array.each{|word| word.reverse!}
+  reversed_array = sentence_array.each{|word| word.reverse!}
+  reversed_array
 end
